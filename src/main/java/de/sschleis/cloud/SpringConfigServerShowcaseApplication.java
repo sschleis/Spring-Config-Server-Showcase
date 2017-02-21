28aff972bbee2c05bedf19898c64e17f74ed1990
sschleis <sebastian.schleis@gmail.com>
2017-02-21 23:09:06 +0100

    Init

---
+++ src/main/java/de/sschleis/cloud/SpringConfigServerShowcaseApplication.java
@@ -0,0 +1,12 @@
+package de.sschleis.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerShowcaseApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sschleis/cloud/SpringConfigServerShowcaseApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sschleis.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConfigServerShowcaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
